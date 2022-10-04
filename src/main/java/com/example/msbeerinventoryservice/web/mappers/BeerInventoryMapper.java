20e5461002bcf28370b276952d28ad7fb54f05d0
cristiii <cristian.vlad10@gmail.com>
2022-10-04 15:48:04 +0300

    configure jms

--- src/main/java/com/example/msbeerinventoryservice/web/mappers/BeerInventoryMapper.java
+++ src/main/java/com/example/msbeerinventoryservice/web/mappers/BeerInventoryMapper.java
@@ -3,10 +3,12 @@ package com.example.msbeerinventoryservice.web.mappers;
 import com.example.msbeerinventoryservice.domain.BeerInventory;
 import com.example.msbeerinventoryservice.web.model.BeerInventoryDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(uses = {DateMapper.class})
 public interface BeerInventoryMapper {
 
+    @Mapping(target = "version", ignore = true)
     BeerInventory beerInventoryDtoToBeerInventory(BeerInventoryDto beerInventoryDTO);
 
     BeerInventoryDto beerInventoryToBeerInventoryDto(BeerInventory beerInventory);
